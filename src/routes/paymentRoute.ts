5bac6d3f1ad16df653392be7d3c16e897781af78
rajanshah23 <shahrajan774@gmail.com>
2025-05-27 20:27:16 +0545

    initial commit

--- src/routes/paymentRoute.ts
+++
@@ -1,9 +0,0 @@
- import express from "express";
-import { initiateKhaltiPayment,verifyKhaltiPayment } from "../controllers/paymentController";
-import { asyncHandler } from "../middleware/authMiddleware";
-
-const router = express.Router();
-
-router.post("/verify",asyncHandler(verifyKhaltiPayment));
-router.post("/initiate", asyncHandler(initiateKhaltiPayment));
-export default router;
