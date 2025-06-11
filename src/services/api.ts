31ad589c7eca1ccae674f6209b36a8b4c89de9e7
rajanshah23 <shahrajan774@gmail.com>
2025-06-11 20:53:57 +0545

    final commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL as string, // use 'as string' to avoid TS errors
+  baseURL: import.meta.env.VITE_API_BASE_URL as string,  
   headers: {
     'Content-Type': 'application/json',
   },
